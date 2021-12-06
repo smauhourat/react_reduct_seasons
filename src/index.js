da48679e76046f4bbce69274c0112da8c9f37cfe
Santiago Mauhourat <smauhourat@aadim.org.ar>
2021-12-06 18:03:20 -0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (module.hot) {
+  module.hot.accept();
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (error) => {
+        this.setState({ errorMessage: error.message });
+      }
+    );    
+  }
+
+  render() {
+    return (
+      <div>
+        Latitude: { this.state.lat }
+        <br />
+        Error: { this.state.errorMessage }
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
