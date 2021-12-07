4146c83efa346e1f555f5d202352bd1fad0bb4b8
Santiago Mauhourat <smauhourat@aadim.org.ar>
2021-12-07 15:05:03 -0300

    update seasons

--- src/index.js
+++ src/index.js
@@ -1,34 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from './SeasonDisplay';
 
 if (module.hot) {
   module.hot.accept();
 }
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   console.log('call constructor');
+  //   super(props);
 
-    this.state = { lat: null, errorMessage: '' };
+  //   this.state = { lat: null, errorMessage: '' };
+  // }
 
+  // Is the same as put in constructor
+  state = { lat: null, errorMessage: '' };
+  
+
+  componentDidMount() {
+    console.log('call componentDidMount');
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (error) => {
-        this.setState({ errorMessage: error.message });
-      }
-    );    
+      position => this.setState({ lat: position.coords.latitude }),
+      error => this.setState({ errorMessage: error.message })
+    );  
   }
 
   render() {
-    return (
-      <div>
-        Latitude: { this.state.lat }
-        <br />
-        Error: { this.state.errorMessage }
-      </div>
-    );
+    console.log('call render');
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat}/>
+    }
+
+    return <div>Loading!</div>;
   }
 }
 
